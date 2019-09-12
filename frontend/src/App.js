c79487f4ba6c17496f290381879c8ad04358d128
CarlW <carl@oskarroger.net>
2019-09-12 21:15:17 +0800

    Front End now fetches data from the Back End and displays it in a list.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,12 @@ import RawDataList from './components/RawDataList';
 // ------- END OF IMPORTS ------------
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000'
+  uri: 'http://localhost:4000/graphql'
 });
 
 const App = () => {
   return (
-    <ApolloProvider>
+    <ApolloProvider client={client}>
       <div className='App'>
         <h1>Hello from React</h1>
         <RawDataList />
--- frontend/src/components/RawDataList.js
+++ frontend/src/components/RawDataList.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const Container = styled.div`
+  border-style: solid;
+  border-color: 'red';
+  margin-bottom: 1rem;
+`;
 
 const RawDataList = () => {
-  return <div>Hello from the list component.</div>;
+  const { loading, error, data } = useQuery(gql`
+    {
+      cpus {
+        company
+        model
+        socket
+      }
+    }
+  `);
+
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  return (
+    <div>
+      {data.cpus.map((x, i) => {
+        return (
+          <Container key={i}>
+            <p>{x.company}</p>
+            <p>{x.model}</p>
+            <p>{x.socket}</p>
+          </Container>
+        );
+      })}
+    </div>
+  );
 };
 
 export default RawDataList;
