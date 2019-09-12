1d1826f1b0365db9e5aed8d8ef0ba32197cc7494
CarlW <carl@oskarroger.net>
2019-09-12 15:29:29 +0800

    Installed Apollo-Client, Preparing for loading data from the Back-End.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import RawDataList from './components/RawDataList';
+
 function App() {
   return (
     <div className='App'>
       <h1>Hello from React</h1>
+      <RawDataList />
     </div>
   );
 }
---
+++ frontend/src/components/RawDataList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RawDataList = () => {
+  return <div>Hello from the list component.</div>;
+};
+
+export default RawDataList;
