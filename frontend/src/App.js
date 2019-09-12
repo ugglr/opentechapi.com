35b14093e6d5c4ad880993e5fa6b37c1e7439cfe
CarlW <carl@oskarroger.net>
2019-09-12 20:22:18 +0800

    Wrapped app with ApolloProvider and connecting to the Back-End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import RawDataList from './components/RawDataList';
 
-function App() {
+// ------- END OF IMPORTS ------------
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+});
+
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-      <RawDataList />
-    </div>
+    <ApolloProvider>
+      <div className='App'>
+        <h1>Hello from React</h1>
+        <RawDataList />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
